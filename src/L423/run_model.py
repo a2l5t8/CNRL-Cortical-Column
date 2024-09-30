4b485885349ec7cd38eb36af8dbb3ad7b788357f
Bita <bita.moharrami@gmail.com>
2024-09-30 16:34:58 +0330

    Final model and parameter tuning added

--- src/L423/run_model.py
+++ src/L423/run_model.py
@@ -32,8 +32,8 @@ IMAGE_HEIGHT = 14
 
 OUT_CHANNEL = 5
 IN_CHANNEL = 1
-KERNEL_WIDTH = 10
-KERNEL_HEIGHT = 10
+KERNEL_WIDTH = 7
+KERNEL_HEIGHT = 7
 
 INPUT_WIDTH = IMAGE_WIDTH - DoG_SIZE + 1
 INPUT_HEIGHT = IMAGE_HEIGHT - DoG_SIZE + 1
@@ -87,9 +87,11 @@ input_layer = InputLayer(
     sensory_size = NeuronDimension(depth=1, height = INPUT_HEIGHT, width = INPUT_WIDTH),
     sensory_trace= 3,
     instance_duration= time_window,
-    silent_interval=100,
+    silent_interval=50,
+    output_ports = {
+        "data_out": (None,[("sensory_pop", {})])
+    }
 )
-
 
 
 
@@ -108,7 +110,7 @@ ng4e = NeuronGroup(size = NeuronDimension(depth = OUT_CHANNEL , height = L4_HEIG
         v_rest = -65,
         v_reset = -70,
     ),
-    KWTA(k=5),
+    KWTA(k=20),
     ActivityBaseHomeostasis(window_size=10, activity_rate=200, updating_rate=0.0001),
     Fire(),
     SpikeTrace(tau_s = 15),
@@ -133,38 +135,29 @@ ng4i = NeuronGroup(size = L4_HEIGHT * L4_WIDTH * OUT_CHANNEL // 4, net = net, ta
 ]))
 
 
-sgi4e = SynapseGroup(net = net, src = input_layer.sensory_pop, dst = ng4e, tag = "Proximal", behavior = prioritize_behaviors([
-    SynapseInit(),
-    WeightInitializer(weights = torch.normal(0.5, 2, (OUT_CHANNEL, IN_CHANNEL, KERNEL_HEIGHT, KERNEL_WIDTH)) ),
-    Conv2dDendriticInput(current_coef = 35 , stride = 1, padding = 0),
-    Conv2dSTDP(a_plus=0.05, a_minus=0.004),
-    WeightNormalization(norm = 50)
-]))
-
-
 sg4e4i = SynapseGroup(net = net, src = ng4e, dst = ng4i, tag = "Proximal", behavior = prioritize_behaviors([
     SimpleDendriticInput(),
     SynapseInit(),
-    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(400 * p), density = 0.02, true_sparsity = False),
+    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(1600 * p), density = 0.02, true_sparsity = False),
 ]))
 
 
 sg4i4e = SynapseGroup(net = net, src = ng4i, dst = ng4e, tag = "Proximal", behavior = prioritize_behaviors([
     SimpleDendriticInput(),
     SynapseInit(),
-    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(400 * p), density = 0.02, true_sparsity = False),
+    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(1600 * p), density = 0.02, true_sparsity = False),
 ]))
 
 sg4e4e = SynapseGroup(net = net, src = ng4e, dst = ng4e, tag = "Proximal", behavior=prioritize_behaviors([
     SynapseInit(),
     WeightInitializer(weights=torch.Tensor([1, 1, 1, 1, 0, 1, 1, 1, 1]).view(1, 1, 9, 1, 1)),
-    LateralDendriticInput(current_coef=1300, inhibitory = True),
+    LateralDendriticInput(current_coef=100000, inhibitory = True),
 ]))
 
 sg4i4i = SynapseGroup(net = net, src = ng4i, dst = ng4i, tag = "Proximal", behavior = prioritize_behaviors([
     SimpleDendriticInput(),
     SynapseInit(),
-    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(400 * p), density = 0.02, true_sparsity = False),
+    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(1600 * p), density = 0.02, true_sparsity = False),
 ]))
 
 
@@ -205,29 +198,102 @@ ng23i = NeuronGroup(size = L23_HEIGHT * L23_WIDTH * OUT_CHANNEL // 4, net = net,
     NeuronAxon(),
 ]))
 
-sg4e23e = SynapseGroup(net = net, src = ng4e, dst = ng23e, tag = "Proximal", behavior = prioritize_behaviors([
-    SynapseInit(),
-    # WeightInitializer(mode = "normal(4, 5)", density = 0.02, true_sparsity = False),
-    AveragePool2D(current_coef = 150),
-]))
-
 sg23e23i = SynapseGroup(net = net, src = ng23e, dst = ng23i, tag = "Proximal", behavior = prioritize_behaviors([
     SimpleDendriticInput(),
     SynapseInit(),
-    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(625 * p), density = 0.02, true_sparsity = False),
+    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(2500 * p), density = 0.02, true_sparsity = False),
 ]))
 
 sg23i23e = SynapseGroup(net = net, src = ng23i, dst = ng23e, tag = "Proximal", behavior = prioritize_behaviors([
     SimpleDendriticInput(),
     SynapseInit(),
-    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(625 * p), density = 0.02, true_sparsity = False),
+    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(2500 * p), density = 0.02, true_sparsity = False),
 ]))
 
 sg23i23i = SynapseGroup(net = net, src = ng23i, dst = ng23i, tag = "Proximal", behavior = prioritize_behaviors([
     SimpleDendriticInput(),
     SynapseInit(),
-    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(625 * p), density = 0.02, true_sparsity = False),
+    WeightInitializer(mode = "ones", scale = J_0/math.sqrt(2500 * p), density = 0.02, true_sparsity = False),
 ]))
+
+
+
+######################## Layers #######################
+
+
+
+L4 = CorticalLayer(
+    net=net,
+    excitatory_neurongroup=ng4e,
+    inhibitory_neurongroup=ng4i,
+    synapsegroups=[sg4e4i, sg4i4e, sg4e4e, sg4i4i],
+    input_ports={
+        "input": (
+            None,
+            [Port(object = ng4e, label = None)],
+        ),
+        "output": (
+            None,
+            [Port(object = ng4e, label = None)]
+        )
+    },
+)
+
+
+
+L23 = CorticalLayer(
+    net=net,
+    excitatory_neurongroup=ng23e,
+    inhibitory_neurongroup=ng23i,
+    synapsegroups=[sg23e23i, sg23i23e, sg23i23i],
+    input_ports={
+        "input": (
+            None,
+            [Port(object = ng23e, label = None)],
+        ),
+        "output": (
+            None,
+            [Port(object = ng23e, label = None)]
+        )
+    },
+)
+
+
+
+############### Inter Layer Connections ###############
+
+
+
+Synapsis_L4_L23 = Synapsis(
+    net = net,
+    src = L4,
+    dst = L23,
+    input_port="output",
+    output_port="input",
+    synapsis_behavior=prioritize_behaviors([
+        SynapseInit(),
+        AveragePool2D(current_coef = 50000),
+    ]),
+    synaptic_tag="Proximal"
+)
+
+
+Synapsis_Inp_L4 = Synapsis(
+    net = net,
+    src = input_layer,
+    dst = L4,
+    input_port="data_out",
+    output_port="input",
+    synapsis_behavior=prioritize_behaviors([
+        SynapseInit(),
+        WeightInitializer(weights = torch.normal(0.1, 2, (OUT_CHANNEL, IN_CHANNEL, KERNEL_HEIGHT, KERNEL_WIDTH)) ),
+        Conv2dDendriticInput(current_coef = 10000 , stride = 1, padding = 0),
+        Conv2dSTDP(a_plus=0.3, a_minus=0.008),
+        WeightNormalization(norm = 10)
+    ]),
+    synaptic_tag="Proximal"
+)
+
 
 
 #######################################################
@@ -238,4 +304,4 @@ sg23i23i = SynapseGroup(net = net, src = ng23i, dst = ng23i, tag = "Proximal", b
 net.initialize()
 net.simulate_iterations(3000)
 
-show_filters(sgi4e.weights)
+show_filters(Synapsis_Inp_L4.synapses[0].weights)
